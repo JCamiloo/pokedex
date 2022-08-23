eb96c8d6849e06882868c834568c0f1fdb5e1b76
JCamiloo <jcamilo.osorio15@gmail.com>
2022-08-22 22:19:23 -0500

    added mongo connection

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,13 @@
-export class Pokemon {}
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Pokemon extends Document {
+  @Prop({ unique: true, index: true })
+  name: string;
+
+  @Prop({ unique: true, index: true })
+  no: number;
+}
+
+export const PockemonSchema = SchemaFactory.createForClass(Pokemon);
--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PockemonSchema, Pokemon } from './entities/pokemon.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Pokemon.name,
+        schema: PockemonSchema,
+      },
+    ]),
+  ],
   controllers: [PokemonController],
-  providers: [PokemonService]
+  providers: [PokemonService],
 })
 export class PokemonModule {}
